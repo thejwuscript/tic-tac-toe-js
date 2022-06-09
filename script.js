30b344fe498d229800f837206fd16e4c86afdbe0
James Wu <ichangjwu@outlook.com>
2022-06-09 22:37:25 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const gameBoard = (() => {
+  const cells = []
+  return {
+    cells
+  };
+})();
+
+const Player = (name, shape) => {
+  const getName = () => name;
+  const getShape = () => shape;
+  return {getName, getShape};
+}
+
+const gameLogic = (() => {
+  // some variables and functions
+  // return an object
+})();
+
+const displayController (() => {
+  // some vars and functions
+  //return an object
+})();
+
